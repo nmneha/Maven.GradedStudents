eba052a1b340d830b3b3cb7350deb4855b99ff45
Nusera <nusera@zipcoders-MacBook-Pro.local>
2022-03-09 08:42:49 -0500

    made changes to add student method in classroom

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -30,7 +30,11 @@ public class Classroom {
     }
 
     public void addStudent(Student student) {
-
+        for (int i = 0; i < students.length; i++) {
+            if (students[i] == null) {
+                students[i] = student;
+            }
+        }
 
     }
 
